02c7d92a0dec46f7995f244fafc4a0ebe6a11c8e
rohit001007 <rohit001007@gmail.com>
2020-11-25 04:07:29 +0530

    Added : Percentage Of Doublet Combination

--- FlipComb.sh
+++ FlipComb.sh
@@ -3,29 +3,44 @@
 count=0
 count1=0
 count2=0
+count3=0
+count4=0
+RANDOM=$$
 
-for (( i=1;i<11; i++))
+for (( i=1;i<19; i++))
 do
-	RANDOM=$$
-	x1=$(( $RANDOM % 10 ))
-	x2=$(( $x1 % 2 ))
-
 	count=$(( $count + 1 ))
+	RANDOM=$(( $RANDOM + $count ))
+	x1=$(( $RANDOM % 4 ))
 
-	if [ $x2 -eq 0 ]
+	if [ $x1 -eq 0 ]
 	then
 		count1=$(( $count1 + 1 ))
 		percent1=$(( ($count1/$count) * 100 ))
 
-	elif [ $x2 -eq 1 ]
+	elif [ $x1 -eq 1 ]
 	then
 		count2=$(( $count2 + 1 ))
 		percent2=$(( ($count2/$count) * 100 ))
+
+	elif [ $x1 -eq 2 ]
+        then
+                count3=$(( $count3 + 1 ))
+                percent3=$(( ($count3/$count) * 100 ))
+
+	 elif [ $x1 -eq 3 ]
+        then
+                count4=$(( $count4 + 1 ))
+                percent4=$(( ($count4/$count) * 100 ))
+
+
 	fi
 done
 
-ar[0]="HEAD : $percent1"
-ar[1]="TAIL : $percent2"
+ar[0]="HH : $percent1"
+ar[1]="HT : $percent2"
+ar[2]="TH : $percent3"
+ar[3]="TT : $percent4"
 
 echo -ne "\n * Array Result With Percentage *\n"
 echo ${ar[@]}
